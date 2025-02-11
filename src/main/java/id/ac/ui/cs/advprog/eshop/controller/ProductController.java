818b37387f42e838f254c0641771da06e23cdf73
JanssenBenedict <janssen.benedict@gmail.com>
2025-02-12 00:52:28 +0700

    Fixed Error Handlings & Updated Reflection 1

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -3,9 +3,11 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,27 +27,21 @@ public class ProductController {
 
     @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model) {
-
-        if (product.getProductName() == null || product.getProductName().trim().isEmpty()) {
-            model.addAttribute("error", "The name of the product cannot be empty or null");
+        try {
+            productValidation(product);
+            service.create(product);
+            return "redirect:list";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
             return "createProduct";
         }
-
-        if (product.getProductQuantity() < 1) {
-            model.addAttribute("error", "The product quantity cannot be negative or zero");
-            return "createProduct";
-        }
-
-        service.create(product);
-        return "redirect:list";
     }
 
     @GetMapping("/edit/{productId}")
     public String editProductPage(@PathVariable String productId, Model model) {
         Product product = service.findById(productId);
         if (product == null) {
-            model.addAttribute("error", "The product cannot be found");
-            return "productList";
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The product cannot be found");
         }
         model.addAttribute("product", product);
         return "editProduct";
@@ -53,19 +49,14 @@ public class ProductController {
 
     @PostMapping("/edit")
     public String editProductPost(@ModelAttribute Product product, Model model) {
-
-        if (product.getProductName() == null || product.getProductName().trim().isEmpty()) {
-            model.addAttribute("error", "The name of the product cannot be empty or null");
+        try {
+            productValidation(product);
+            service.update(product);
+            return "redirect:/product/list";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
             return "editProduct";
         }
-
-        if (product.getProductQuantity() < 1) {
-            model.addAttribute("error", "The product quantity cannot be negative or zero");
-            return "editProduct";
-        }
-
-        service.update(product);
-        return "redirect:/product/list";
     }
 
     @GetMapping("/list")
@@ -77,14 +68,22 @@ public class ProductController {
 
     @GetMapping("/delete/{productId}")
     public String deleteProduct(@PathVariable String productId, Model model) {
-
         Product product = service.findById(productId);
         if (product == null) {
-            model.addAttribute("error", "The product cannot be not found");
-            return "productList";
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The product cannot be found");
         }
-
         service.delete(productId);
         return "redirect:/product/list";
+    }
+
+    private void productValidation(Product product) {
+
+        if (product.getProductName() == null || product.getProductName().trim().isEmpty()) {
+            throw new IllegalArgumentException("The name of the product cannot be empty or null");
+        }
+
+        if (product.getProductQuantity() < 1) {
+            throw new IllegalArgumentException("The product quantity cannot be negative or zero");
+        }
     }
 }

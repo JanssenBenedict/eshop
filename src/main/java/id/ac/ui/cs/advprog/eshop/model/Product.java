affe134982bec077892849b561ac453cb515cc57
JanssenBenedict <janssen.benedict@gmail.com>
2025-02-12 10:48:48 +0700

    Fixed Outdated Product Model Code

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,19 +3,19 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Min;
+
+import java.util.UUID;
 
 @Getter @Setter
 public class Product {
     private String productId;
 
-    @NotBlank(message = "Product name is required")
+    @NotBlank(message = "The name of the product is needed")
     private String productName;
 
-    @Min(value = 1, message = "Quantity must be at least 1")
+    @Min(value = 1, message = "The product quantity cannot be negative or zero")
     private int productQuantity;
 
     public Product() {

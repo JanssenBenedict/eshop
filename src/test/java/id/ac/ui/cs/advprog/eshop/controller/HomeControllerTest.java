b574150312d816b68439f02fd833cad92bee6643
JanssenBenedict <janssen.benedict@gmail.com>
2025-02-17 20:44:16 +0700

    Explicitly import Thymeleaf templates

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -2,12 +2,17 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
+@AutoConfigureMockMvc
+@Import(ThymeleafAutoConfiguration.class)
 class HomeControllerTest {
 
     @Autowired

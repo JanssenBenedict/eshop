09c67babf6185ad96a305e4f9e86fd6f6373311e
JanssenBenedict <janssen.benedict@gmail.com>
2025-02-22 16:50:17 +0700

    Fixed Unit Test Error

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -80,7 +80,7 @@ public class ProductController {
 
 @Controller
 @RequestMapping("/car")
-class CarController extends ProductController {
+class CarController {
 
     @Autowired
     private CarServiceImpl carservice;
